c653001e206a6543a9244d2bb9ce06c63da8310f
MrW24 <swfa01@zoho.com>
2016-04-24 16:47:42 -0230

    Added and deleted a few things

--- plugins/datafiles.py
+++ plugins/datafiles.py
@@ -27,16 +27,8 @@ with open("plugins/data/flirts.txt") as f:
     flirts = [line.strip() for line in f.readlines() if not line.startswith("//")]
 
 
-with open("plugins/data/yiffs.txt") as f:
-    yiffs = [line.strip() for line in f.readlines() if not line.startswith("//")]
-
-
-with open("plugins/data/lewd.txt") as f:
+with open("plugins/data/fishboles.txt") as f:
     lewds = [line.strip() for line in f.readlines() if not line.startswith("//")]
-
-with open("plugins/data/slogans.txt") as f:
-    slogans = [line.strip() for line in f.readlines() if not line.startswith("//")]
-
 
 
 def get_generator(_json, variables):
@@ -85,27 +77,10 @@ def insult(inp, me=None, nick=None, conn=None, notice=None, chan=None):
     return
 
 
-@hook.command
-def flirt(inp, me=None, nick=None, conn=None, notice=None, chan=None):
-    """flirt <user> -- Makes the bot flirt <user>."""
-    send_phrase(inp,flirts,nick,conn,me,notice, chan)
-    return
-
-
 @hook.command(autohelp=False)
 def yiff(inp, me=None, nick=None, conn=None, notice=None, chan=None):
     """yiff <user> -- yiffs <user>."""
     send_phrase(inp,yiffs,nick,conn,me,notice, chan)
-    return
-
-
-@hook.command(autohelp=False)
-def lewd(inp, me=None, nick=None, conn=None, notice=None, chan=None):
-    """lewd <user> -- lewd <user>."""
-    if len(inp) == 0:
-        return 'ヽ(◔ ◡ ◔)ノ.･ﾟ*｡･+☆LEWD☆'.decode('UTF-8')
-    else:
-        send_phrase(inp,lewds,nick,conn,me,notice, chan)
     return
 
 
@@ -158,15 +133,53 @@ def slap(inp, me=None, nick=None, conn=None, notice=None):
     me(generator.generate_string())
     return
 
+@hook.command
+def cookie(inp, me=None, nick=None, conn=None, notice=None):
+    """cookie <user> -- Gives <user> a cookie."""
+    target = inp.strip()
+
+    if " " in target:
+        notice("Invalid username!")
+        return
+
+    # if the user is trying to make the bot cookie itself, cookie them
+    if target.lower() == conn.nick.lower() or target.lower() == "itself":
+        target = nick
+
+    variables = {
+        "user": target
+    }
+
+    with open("plugins/data/cookies.json") as f:
+        generator = get_generator(f.read(), variables)
+
+    # act out the message
+    me(generator.generate_string())
+    return
 
 @hook.command
-def slogan(inp):
-    """slogan <word> -- Makes a slogan for <word>."""
-    out = random.choice(slogans)
-    if inp.lower() and out.startswith("<text>"):
-        inp = text.capitalize_first(inp)
+def rekt(inp, me=None, nick=None, conn=None, notice=None):
+    """rekt <user> -- Makes the bot rekt <user>."""
+    target = inp.strip()
 
-    return out.replace('<text>', inp)
+    if " " in target:
+        notice("Invalid username!")
+        return
+
+    # if the user is trying to make the bot rekt itself, rekt them
+    if target.lower() == conn.nick.lower() or target.lower() == "itself":
+        target = nick
+
+    variables = {
+        "user": target
+    }
+
+    with open("plugins/data/rekt.json") as f:
+        generator = get_generator(f.read(), variables)
+
+    # act out the message
+    me(generator.generate_string())
+    return
 
 
 def get_filename(action,notice):
@@ -181,10 +194,11 @@ def get_filename(action,notice):
     elif 'honry' in action: action = 'old'
     elif 'old' in action: action = 'old'
     elif 'fortune' in action: action = 'fortunes'
-    elif 'slogan' in action: action = 'slogans'
+    elif 'fishbole' in action: action = 'fishboles'
     elif 'troll' in action: action = 'troll'
     elif 'gain' in action: action = 'gainz'
     elif 'nsfw' in action: action = 'nsfw'
+    elif 'rekt' in action: action = 'rekt'
     else:
         notice('Invalid action')
         return
@@ -288,64 +302,17 @@ def fortune(inp,say=None,notice=None):
     say(process_text(inp,"fortunes",notice))
     return
 
-@hook.command("kek", autohelp=False)
+@hook.command("fishbole", autohelp=False)
 @hook.command(autohelp=False)
-def topkek(inp,say=None,notice=None):
-    """topkek -- keks on demand."""
-    say(process_text(inp,"keks",notice))
+def fishbole(inp,say=None,notice=None):
+    """fishbole -- Displays a random fishbole quote."""
+    say(process_text(inp,"fishboles",notice))
     return
 
-@hook.command(autohelp=False)
-def loli(inp,say=None,notice=None):
-    """loli -- Returns a loli."""
-    say("\x02\x034NSFW\x03\x02 {}".format(process_text(inp,"lolis",notice)))
-    return
 
-@hook.command(autohelp=False)
-def moistcake(inp,say=None,notice=None):
-    "moistcake -- Moists on demand."
-    say(process_text(inp,"moists",notice))
-    return
 
-@hook.command(autohelp=False)
-def qt(inp,me=None,say=None,notice=None):
-    """qt --  return qts."""
-    say(process_text(inp,"qts",notice))
-    return
 
-@hook.command(autohelp=False)
-def urmom(inp,me=None,say=None,notice=None):
-    """urmom --  return urmom."""
-    say(process_text(inp,"urmom",notice))
-    return
 
-@hook.command("old", autohelp=False)
-@hook.command(autohelp=False)
-def honry(inp,me=None,say=None,notice=None):
-    """honry --  return honry."""
-    say(process_text(inp,"old",notice))
-    return
-
-@hook.command(autohelp=False)
-def bender(inp,say=None):
-    """bender -- Bender quotes."""
-    benders = ["Bite my shiny, metal ass!", "Bite my glorious, golden ass!", "Bite my shiny, colossal ass!", "Bite my splintery, wooden ass!", "Lick my frozen, metal ass!", "Like most of life's problems, this one can be solved with bending.", "Cheese it!", "Well, I'm boned.", "Hey, sexy mama...wanna kill all humans?", "Oh! Your! God!", "He's pending for a bending!", "This is the worst kind of discrimination - the kind against me!", "In case of emergency, my ass can be used as a flotation device.", "In order to get busy at maximum efficiency, I need a girl with a big, 400-ton booty.", "I'm sick of shaking my booty for these fat jerks!", "Bite my red-hot glowing ass!", "All I know is, this gold says it was the best mission ever.", "Hey, guess what you're all accessories to.", "Well, I don't have anything else planned for today. Let's get drunk!", "Oh, no room for Bender, huh? Fine! I'll go build my own lunar lander! With blackjack and hookers! In fact, forget the lunar lander and the blackjack! Ah, screw the whole thing.", "I found it in the street! Like all the food I cook.", "I can't stand idly by while poor people get free food!", "Congratulations Fry, you've snagged the perfect girlfriend. Amy's rich, she's probably got other characteristics...", "You may need to metaphorically make a deal with the devil. By 'devil' I mean robot devil and by 'metaphorically' I mean get your coat.", "Boy, who knew a cooler could also make a handy wang coffin?", "Call me old fashioned but I like a dump to be as memorable as it is devastating.", "My life, and by extension everyone else's is meaningless.", "Do I preach to you while you're lying stoned in the gutter? No.", "Everybody's a jerk. You, me, this jerk.", "I hate the people that love me and they hate me.", "I've personalized each of your meals. Amy, you're cute, so I baked you a pony!", "Ahh, computer dating. It's like pimping, but you rarely have to use the phrase, 'upside your head'.", "Court’s kinda fun when it’s not my ass on the line!", "Maybe you can interface with my ass! By biting it!", "Well, I'll go build my own theme park! With blackjack and hookers! In fact, forget the park!", "  Compare your lives to mine and then kill yourself!", "I would give up my 8 other senses, even smision, for a sense of taste!", "Stupid anti-pimping laws!", "Blackmail’s such an ugly word. I prefer extortion. The x makes it sound cool.", "Great is ok, but amazing would be great!", "The pie is ready. You guys like swarms of things, right?", "Fry cracked corn, and I don't care; Leela cracked corn, I still don't care; Bender cracked corn, and he is great! Take that you stupid corn!", "Stay away from our women. You got metal fever, baby, metal fever!", "If it ain't black and white, peck, scratch and bite.", "Life is hilariously cruel.", "Pardon me, brother. Care to donate to the anti-mugging you fund?", "I love this planet. I've got wealth, fame, and access to the depths of sleaze that those things bring.", "C'mon, it's just like making love. Y'know, left, down, rotate sixty-two degrees, engage rotors...", "Oh my God, I'm so excited I wish I could wet my pants.", "Argh. The laws of science be a harsh mistress.", "In the event of an emergency, my ass can be used as a floatation device.", "Hey, I got a busted ass here! I don't see anyone kissing it.", "I'm a fraud - a poor, lazy, sexy fraud.", "This'll show those filthy bastards who's loveable!"]
-    say(random.choice(benders))
-    benders = []
-    return
-
-@hook.command('gains', autohelp=False)
-@hook.command(autohelp=False)
-def gainz(inp, say=None,notice=None):
-    """gains -- SICK GAINZ BRO"""
-    say(process_text(inp,"gainz",notice))
-    return
-
-@hook.command(autohelp=False)
-def nsfw(inp, say=None,notice=None):
-    """nsfw -- Have a nice fap"""
-    say(process_text(inp,"nsfw",notice))
-    return
 
 
 
